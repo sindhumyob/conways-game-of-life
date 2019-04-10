6ac933281d351fa304702a73e75d65e3c5726b91
sindhumyob <sindhu.singamneni@myob.com>
2019-04-11 10:00:26 +1200

    Implemented game output messages

---
+++ ConwaysGameOfLife/GamePlayHelpers/GameOutput.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+
+namespace ConwaysGameOfLife
+{
+    public class GameOutput
+    {
+        public string WelcomeMessage()
+        {
+            return "Welcome to Conway's Game of Life!\n";
+        }
+
+        public string EnterGridHeightMessage()
+        {
+            return "Please enter the height of your game grid or quit the game with 'q': ";
+        }
+
+        public string EnterGridWidthMessage()
+        {
+            return "Please enter the width of your game grid or quit the game with 'q': ";
+        }
+
+        public string InvalidGridSizeMessage()
+        {
+            return "Please enter a valid grid size input consisting of a single positive whole number\n";
+        }
+
+        public string PrintGridMessage(char[,] grid)
+        {
+            return "Perfect, here is the grid:\n" + PrintGrid(grid);
+        }
+
+        public string InvalidCoordinateMessage(int maxSizeOfCoordinate)
+        {
+            return "Please enter a valid coordinate input consisting of a number between 1 and " + maxSizeOfCoordinate +
+                   "\n";
+        }
+
+        public string AddInitialSeedMessage()
+        {
+            return "It's now time to add the seed of the system:\n";
+        }
+
+        public string EnterXCoordinateOfCellMessage()
+        {
+            return "Please enter the x coordinate of the cell in the seed or quit the game with 'q': ";
+        }
+
+        public string EnterYCoordinateOfCellMessage()
+        {
+            return "Please enter the y coordinate of the cell in the seed or quit the game with 'q': ";
+        }
+
+        public string AddMoreLiveCellsMessage()
+        {
+            return "Would you like to add more live cells? (y/n) or quit the game with 'q': ";
+        }
+
+        public string InvalidAddMoreLiveCellsMessage()
+        {
+            return
+                "Please enter a valid input consisting of either 'y' for adding more cells or 'n' for starting game or 'q' for quitting\n";
+        }
+
+        public string StartingGameOfLifeMessage()
+        {
+            return "It's now time to start the game of life!\n";
+        }
+
+        public string PrintNextGenerationGridMessage(char[,] grid)
+        {
+            return "Here's the next generation:\n" + PrintGrid(grid);
+        }
+
+        private string PrintGrid(char[,] initialGrid)
+        {
+            var output = new List<string>();
+            var line = string.Empty;
+            for (var i = 0; i < initialGrid.GetLength(0); i++)
+            {
+                for (var j = 0; j < initialGrid.GetLength(1); j++)
+                {
+                    line += initialGrid[i, j] + " ";
+                }
+
+                output.Add(line);
+                line = string.Empty;
+            }
+
+            return string.Join("\n", output) + "\n";
+        }
+    }
+}
---
+++ ConwaysGameOfLifeTests/GamePlayHelpers/GameOutputShould.cs
@@ -0,0 +1,184 @@
+using System.Collections.Generic;
+using ConwaysGameOfLife;
+using Xunit;
+
+namespace ConwaysGameOfLifeTests
+{
+    public class GameOutputShould
+    {
+        private readonly GameOutput _gameOutput;
+
+        public GameOutputShould()
+        {
+            _gameOutput = new GameOutput();
+        }
+
+        [Fact]
+        public void Return_Game_Welcome_Message()
+        {
+            Assert.Equal("Welcome to Conway's Game of Life!\n", _gameOutput.WelcomeMessage());
+        }
+
+        [Fact]
+        public void Return_Enter_Grid_Height_Message()
+        {
+            Assert.Equal("Please enter the height of your game grid or quit the game with 'q': ",
+                _gameOutput.EnterGridHeightMessage());
+        }
+
+        [Fact]
+        public void Return_Enter_Grid_Width_Message()
+        {
+            Assert.Equal("Please enter the width of your game grid or quit the game with 'q': ",
+                _gameOutput.EnterGridWidthMessage());
+        }
+
+        [Fact]
+        public void Return_Invalid_Grid_Size_Message()
+        {
+            Assert.Equal("Please enter a valid grid size input consisting of a single positive whole number\n",
+                _gameOutput.InvalidGridSizeMessage());
+        }
+
+        [Fact]
+        public void Return_Current_Grid_During_Player_Input_Message()
+        {
+            var initialGrid = new[,]
+            {
+                {
+                    (char) CellType.Dead, (char) CellType.Dead, (char) CellType.Dead, (char) CellType.Dead,
+                    (char) CellType.Dead, (char) CellType.Dead
+                },
+                {
+                    (char) CellType.Dead, (char) CellType.Dead, (char) CellType.Dead, (char) CellType.Dead,
+                    (char) CellType.Dead, (char) CellType.Dead
+                },
+                {
+                    (char) CellType.Dead, (char) CellType.Dead, (char) CellType.Dead, (char) CellType.Dead,
+                    (char) CellType.Dead, (char) CellType.Dead
+                },
+                {
+                    (char) CellType.Dead, (char) CellType.Dead, (char) CellType.Dead, (char) CellType.Dead,
+                    (char) CellType.Dead, (char) CellType.Dead
+                },
+                {
+                    (char) CellType.Dead, (char) CellType.Dead, (char) CellType.Dead, (char) CellType.Dead,
+                    (char) CellType.Dead, (char) CellType.Dead
+                },
+                {
+                    (char) CellType.Dead, (char) CellType.Dead, (char) CellType.Dead, (char) CellType.Dead,
+                    (char) CellType.Dead, (char) CellType.Dead
+                },
+            };
+
+            var expectedMessage = "Perfect, here is the grid:\n" + PrintGrid(initialGrid);
+            Assert.Equal(expectedMessage, _gameOutput.PrintGridMessage(initialGrid));
+        }
+
+        [Fact]
+        public void Return_Adding_Initial_Seed_Message()
+        {
+            Assert.Equal("It's now time to add the seed of the system:\n", _gameOutput.AddInitialSeedMessage());
+        }
+
+        [Fact]
+        public void Return_Enter_X_Coordinate_Of_Cell_In_Seed_Message()
+        {
+            Assert.Equal("Please enter the x coordinate of the cell in the seed or quit the game with 'q': ",
+                _gameOutput.EnterXCoordinateOfCellMessage());
+        }
+
+        [Fact]
+        public void Return_Enter_Y_Coordinate_Of_Cell_In_Seed_Message()
+        {
+            Assert.Equal("Please enter the y coordinate of the cell in the seed or quit the game with 'q': ",
+                _gameOutput.EnterYCoordinateOfCellMessage());
+        }
+
+        [Fact]
+        public void Return_Invalid_Coordinate_Message()
+        {
+            var heightOfBoard = 20;
+            Assert.Equal(
+                "Please enter a valid coordinate input consisting of a number between 1 and 20\n",
+                _gameOutput.InvalidCoordinateMessage(heightOfBoard));
+        }
+
+        [Fact]
+        public void Return_Add_More_Live_Cells_Message()
+        {
+            Assert.Equal(
+                "Would you like to add more live cells? (y/n) or quit the game with 'q': ",
+                _gameOutput.AddMoreLiveCellsMessage());
+        }
+
+        [Fact]
+        public void Return_Invalid_Add_More_Live_Cells_Message()
+        {
+            Assert.Equal(
+                "Please enter a valid input consisting of either 'y' for adding more cells or 'n' for starting game or 'q' for quitting\n",
+                _gameOutput.InvalidAddMoreLiveCellsMessage());
+        }
+        
+        [Fact]
+        public void Return_Starting_Game_Of_Life_Message()
+        {
+            Assert.Equal(
+                "It's now time to start the game of life!\n",
+                _gameOutput.StartingGameOfLifeMessage());
+        }
+        
+        [Fact]
+        public void Return_Next_Generation_Grid_Message()
+        {
+            var grid = new[,]
+            {
+                {
+                    (char) CellType.Dead, (char) CellType.Dead, (char) CellType.Dead, (char) CellType.Dead,
+                    (char) CellType.Dead, (char) CellType.Dead
+                },
+                {
+                    (char) CellType.Dead, (char) CellType.Dead, (char) CellType.Dead, (char) CellType.Live,
+                    (char) CellType.Dead, (char) CellType.Dead
+                },
+                {
+                    (char) CellType.Dead, (char) CellType.Live, (char) CellType.Dead, (char) CellType.Dead,
+                    (char) CellType.Live, (char) CellType.Dead
+                },
+                {
+                    (char) CellType.Dead, (char) CellType.Live, (char) CellType.Dead, (char) CellType.Dead,
+                    (char) CellType.Live, (char) CellType.Dead
+                },
+                {
+                    (char) CellType.Dead, (char) CellType.Dead, (char) CellType.Live, (char) CellType.Dead,
+                    (char) CellType.Dead, (char) CellType.Dead
+                },
+                {
+                    (char) CellType.Dead, (char) CellType.Dead, (char) CellType.Dead, (char) CellType.Dead,
+                    (char) CellType.Dead, (char) CellType.Dead
+                },
+            };
+
+            var expectedMessage = "Here's the next generation:\n" + PrintGrid(grid);
+            Assert.Equal(expectedMessage, _gameOutput.PrintNextGenerationGridMessage(grid));
+        }
+
+        private string PrintGrid(char[,] initialGrid)
+        {
+            var output = new List<string>();
+            var line = string.Empty;
+            for (var i = 0; i < initialGrid.GetLength(0); i++)
+            {
+                for (var j = 0; j < initialGrid.GetLength(1); j++)
+                {
+                    line += initialGrid[i, j] + " ";
+                }
+
+                output.Add(line);
+                line = string.Empty;
+            }
+
+            return string.Join("\n", output) + "\n";
+        }
+    }
+}
