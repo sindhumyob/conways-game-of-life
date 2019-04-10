682433acc93894e3c282d8f5312347a8178ecf61
sindhumyob <sindhu.singamneni@myob.com>
2019-04-11 11:07:00 +1200

    Implemented input validation logic

--- ConwaysGameOfLife/GameInput/InputValidator.cs
+++ ConwaysGameOfLife/GameInput/InputValidator.cs
@@ -1,10 +1,36 @@
+using System.Linq;
+
 namespace ConwaysGameOfLife.GameInput
 {
     public class InputValidator
     {
         public bool IsGridSizeResponseValid(string input)
         {
-            throw new System.NotImplementedException();
+            if (input.ToLower() == "q")
+            {
+                return true;
+            }
+
+            if (!int.TryParse(input, out var n)) return false;
+
+            return int.Parse(input) > 0;
+        }
+
+        public bool IsCoordinateResponseValid(string input, int maxGridInputSize)
+        {
+            if (input.ToLower() == "q")
+            {
+                return true;
+            }
+
+            if (!int.TryParse(input, out var n)) return false;
+
+            return int.Parse(input) >= 0 && int.Parse(input) <= maxGridInputSize;
+        }
+
+        public bool IsAddMoreLiveCellsResponseValid(string input)
+        {
+            return input.ToLower() == "q" || input.ToLower() == "y" ||input.ToLower() == "n";
         }
     }
 }
--- ConwaysGameOfLifeTests/GameInputTests/InputValidatorShould.cs
+++ ConwaysGameOfLifeTests/GameInputTests/InputValidatorShould.cs
@@ -16,10 +16,74 @@ namespace ConwaysGameOfLifeTests
         [InlineData("10")]
         [InlineData("8")]
         [InlineData("q")]
+        [InlineData("Q")]
         public void Return_True_When_Grid_Size_Response_Is_Valid(string input)
         {
             var returned = _inputValidator.IsGridSizeResponseValid(input);
             Assert.True(returned);
+        }
+        
+        [Theory]
+        [InlineData("")]
+        [InlineData("-50")]
+        [InlineData("0")]
+        [InlineData("quit")]
+        [InlineData("$%#$#")]
+        public void Return_False_When_Grid_Size_Response_Is_Not_Valid(string input)
+        {
+            var returned = _inputValidator.IsGridSizeResponseValid(input);
+            Assert.False(returned);
+        }
+        
+        [Theory]
+        [InlineData("3")]
+        [InlineData("0")]
+        [InlineData("15")]
+        [InlineData("q")]
+        public void Return_True_When_Input_Coordinate_For_Cell_Is_Valid(string input)
+        {
+            var maxGridInputSize = 15;
+            var returned = _inputValidator.IsCoordinateResponseValid(input, maxGridInputSize);
+            Assert.True(returned);
+        }
+        
+        [Theory]
+        [InlineData("")]
+        [InlineData("-15")]
+        [InlineData("16")]
+        [InlineData("quit")]
+        [InlineData("$%#$#")]
+        public void Return_False_When_Input_Coordinate_For_Cell_Is_Not_Valid(string input)
+        {
+            var maxGridInputSize = 15;
+            var returned = _inputValidator.IsCoordinateResponseValid(input, maxGridInputSize);
+            Assert.False(returned);
+        }
+        
+        [Theory]
+        [InlineData("y")]
+        [InlineData("n")]
+        [InlineData("Y")]
+        [InlineData("N")]
+        [InlineData("q")]
+        [InlineData("Q")]
+        public void Return_True_When_Adding_More_Live_Cells_Input_Is_Valid(string input)
+        {
+            var returned = _inputValidator.IsAddMoreLiveCellsResponseValid(input);
+            Assert.True(returned);
+        }
+        
+        [Theory]
+        [InlineData("")]
+        [InlineData("Yes")]
+        [InlineData("no")]
+        [InlineData("quit")]
+        [InlineData("$%#$#")]
+        [InlineData("3")]
+        public void Return_False_When_Adding_More_Live_Cells_Input_Is_Not_Valid(string input)
+        {
+            var returned = _inputValidator.IsAddMoreLiveCellsResponseValid(input);
+            Assert.False(returned);
         }
     }
 }
