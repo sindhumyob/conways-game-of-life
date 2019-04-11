6779646655e48af866a2d5e20f3a7c03dd981c3f
sindhumyob <sindhu.singamneni@myob.com>
2019-04-12 09:35:19 +1200

    Added stub for getting player input

---
+++ ConwaysGameOfLife/GameInput/GameInput.cs
@@ -0,0 +1,13 @@
+using System;
+using ConwaysGameOfLife.GameInput.Interfaces;
+
+namespace ConwaysGameOfLife.GameInput
+{
+    public class GameInput:IGameInput
+    {
+        public string GetPlayerInput()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ ConwaysGameOfLifeTests/Stubs/GameInput.cs
@@ -0,0 +1,15 @@
+using ConwaysGameOfLife.GameInput.Interfaces;
+
+namespace ConwaysGameOfLifeTests.Stubs
+{
+    public class GameInput : IGameInput
+    {
+        public string PlayerInput { get; set; }
+        public string GetPlayerInput()
+        {
+            return PlayerInput;
+        }
+
+        
+    }
+}
