1d04dbb4a720178a3fbd11c0e9daa064fba4fa8e
sindhumyob <sindhu.singamneni@myob.com>
2019-04-23 10:36:42 +1200

    Removed implementation test

--- ConwaysGameOfLifeTests/PlayGameOfLifeTests/GameGridShould.cs
+++ ConwaysGameOfLifeTests/PlayGameOfLifeTests/GameGridShould.cs
@@ -15,31 +15,7 @@ namespace ConwaysGameOfLifeTests.PlayGameOfLifeTests
         }
 
         [Fact]
-        public void Generate_Game_Grid_With_Dimensions_That_Resembles_Input_Grid_Size()
-        {
-            const int heightOfGrid = 3;
-            const int widthOfGrid = 3;
-
-            _gameGrid.GenerateInitialGrid(heightOfGrid, widthOfGrid);
-
-            var expectedGrid = new[,]
-            {
-                {
-                    CellType.Dead, CellType.Dead, CellType.Dead
-                },
-                {
-                    CellType.Dead, CellType.Dead, CellType.Dead
-                },
-                {
-                    CellType.Dead, CellType.Dead, CellType.Dead
-                }
-            };
-
-            Assert.Equal(expectedGrid, _gameGrid.CurrentGameGrid);
-        }
-
-        [Fact]
-        public void Update_Game_Grid_With_Specified_Live_Cells_And_Dead_Cells()
+        public void Generate_And_Update_Game_Grid_With_Specified_Live_Cells_And_Dead_Cells()
         {
             const int heightOfGrid = 6;
             const int widthOfGrid = 6;
